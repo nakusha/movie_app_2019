efa97db181aa68429765f5ce670e0080916b910e
정연수 <jeong-yeonsu@jeong-yeonsuui-Macmini.local>
2019-09-30 23:38:22 +0900

    2.0 Creating React Component React has only one Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Test from "./test";
 function App() {
-  return <div className="App" />
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Test/>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/test.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Test(){
+    return <h3>React Test</h3>
+}
+
+export default Test;
