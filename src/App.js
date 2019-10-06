59d90df9be197dbe6b43fbbe039dafcd0dd89255
정연수 <jeong-yeonsu@jeong-yeonsuui-Macmini.local>
2019-10-06 23:38:32 +0900

    2.2 using object show dynamic component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
-function Food({ fav }){
-  return <h1>I Like {fav}</h1>
+// function Foor(prop.fav)
+function Food({ name, picture }){
+  return <div>
+      <h2>I Like {name}</h2>
+      <img src={ picture }/>
+    </div>
 }
+
+const foodILike = [
+  {
+    name:"kimchi",
+    image:"https://cms.splendidtable.org/sites/default/files/styles/w2000/public/6878019331_c6d21380b9_z.jpg?itok=QPCBvQxa"
+  },
+  {
+    name:"ramen",
+    image:"https://img1.daumcdn.net/thumb/R720x0/?fname=https://t1.daumcdn.net/liveboard/dailylife/222d88e5c7dc496c8e8a8a56c3452e52.JPG"
+  }
+]
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      <Food 
-        fav="kimchi"/>
+      {foodILike.map(dish => (
+        <Food name={ dish.name } picture={ dish.image }/>
+      ))}
     </div>
   );
 }
