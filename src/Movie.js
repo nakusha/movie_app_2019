467032900c8944e66b85f322623817984c5a79e2
정연수 <jeong-yeonsu@jeong-yeonsuui-Macmini.local>
2019-10-10 00:03:58 +0900

    4.4 apply css

--- src/Movie.js
+++ src/Movie.js
@@ -4,13 +4,13 @@ import "./Movie.css"
 
 
 function Movie({ year, title, summary, poster, genres }){
-    return <div className="movies__movie">
+    return <div className="movie">
             <img src={ poster } alt={ title } title={ title }/>
         <div className="movie__data">
             <h3 className="movie__title">{ title }</h3>
             <h5 className="movie__year">{ year }</h5>
             <p className="movie__summary">{ summary }</p>
-            <ul className="genres">
+            <ul className="movie__genres">
                 { genres.map((genre, index) => (
                     <li key={index} className="genres__genre">{ genre }</li>
                 )) }
